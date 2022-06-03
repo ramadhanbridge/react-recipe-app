03a86d7d21296dc485a13d2d5f3c525b155bf5aa
ramadhanbridge <nyiringondoramadan@gmail.com>
2022-06-03 05:39:03 +0200

    layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-
+import Home from "./pages/Home";
 function App() {
   return (
     <div className="App">
-        <h1>Hello Ram</h1>
+      <h1>Hello Ram</h1>
+      <Home />
     </div>
   );
 }
---
+++ src/components/Veggie.js
@@ -0,0 +1,12 @@
+import Popular from "./Popular"
+
+function Veggie() {
+    return (
+        <div>
+            <Veggie/>
+            <Popular/>
+        </div>
+    )
+}
+
+export default Veggie
